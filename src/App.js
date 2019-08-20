0b3ce9747c5220fab45554397467ad4d6e5ce97d
Tiago <tprado10@icloud.com>
2019-08-20 14:27:25 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import Game from './components/Game';
+
+class App extends Component {
+  render() {
+    return (
+      <Game />
+    );
+  }
+}
+
+export default App;
